684dea8271acc75d3f5808f9c6ec659d802d9da2
Văn Đạt <levandat21082005@gmail.com>
2026-02-06 10:12:08 +0700

    Thiết lập chia bố cục layout

---
+++ resources/views/Admin/partials/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light py-3 px-2">
+    <div class="container-fluid">
+        <button class="btn btn-outline-secondary btn-sm" id="menu-toggle"><i class="fas fa-bars"></i></button>
+        <div class="input-search col-3 d-flex px-4 gap-2">
+            <input type="" class="form-control">
+            <button class="btn btn-primary"><i class="fa fa-search"></i></button>
+
+        </div>
+
+        <div class="ms-auto d-flex align-items-center">
+
+            <div class="me-3 mt-1"><i class="far fa-bell fs-5 text-muted"></i></div>
+            <img src="https://tse4.mm.bing.net/th/id/OIP.ZPuMm81FSMbBbt-Xaa3aQAHaHa?pid=Api&P=0&h=180"
+                style="width: 40px; height: 40px;" class="rounded-circle me-2">
+            <span class="fw-bold">John Doe</span>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/Admin/partials/sidebar.blade.php
@@ -0,0 +1,14 @@
+<div id="sidebar-wrapper" class="bg-light">
+    <div class="sidebar-heading p-3 fs-4 text-body fw-bold "><i class="fas fa-user-shield text-primary"></i>
+        Adminator</div>
+    <div class="nav flex-column py-3  ">
+        <a href="#" class="nav-link text-muted fw-bold  "><i class="fas fa-tachometer-alt"></i> Dashboard</a>
+        <a href="#" class="nav-link text-muted fw-bold"><i class="fas fa-box"></i> Quản lý Danh mục</a>
+        <a href="#" class="nav-link text-muted fw-bold"><i class="fas fa-box"></i> Quản lý Sản phẩm</a>
+        <a href="#" class="nav-link text-muted fw-bold"><i class="fas fa-shopping-cart"></i> Đơn hàng</a>
+        <!-- <a href="#" class="nav-link text-muted fw-bold"><i class="fas fa-chart-bar"></i> Báo cáo doanh thu</a> -->
+        <a href="#" class="nav-link text-muted fw-bold"><i class="fas fa-users"></i> Quản lý User</a>
+        <a href="#" class="nav-link text-muted fw-bold mt-auto border-0"><i class="fas fa-sign-out-alt"></i>Đăng
+            xuất</a>
+    </div>
+</div>
