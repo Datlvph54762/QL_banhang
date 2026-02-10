7717cd0e0a3ca33cc7eebe5dcd92ceb06e3e2179
Văn Đạt <levandat21082005@gmail.com>
2026-02-10 10:33:58 +0700

    Thêm hiển thị thông báo thành công

--- resources/views/Admin/Categories/index.blade.php
+++ resources/views/Admin/Categories/index.blade.php
@@ -6,9 +6,18 @@
     <div class="card border-0 shadow-sm">
         <div class="card-header bg-white py-3 d-flex justify-content-between align-items-center">
             <h5 class="m-0 fw-bold text-primary">Danh sách Danh mục</h5>
-            <a href="{{ route('admin.categories.create') }}" class="btn btn-sm btn-primary"><i class="fas fa-plus"></i> Thêm mới</a>
+            <a href="{{ route('admin.categories.create') }}" class="btn btn-sm btn-primary"><i class="fas fa-plus"></i> Thêm
+                mới</a>
         </div>
         <div class="card-body">
+            @if (session('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    <ul class="mb-0">
+                        <li>{{ session('success') }}</li>
+                    </ul>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
             <div class="table-responsive">
                 <table class="table table-hover align-middle">
                     <thead class="table-secondary">
@@ -24,7 +33,7 @@
                             <tr>
                                 <td>{{ $category->id }}</td>
                                 <td>{{ $category->name }}</td>
-                                <td><span >{{ $category->description }}</span></td>
+                                <td><span>{{ $category->description }}</span></td>
                                 <td class="text-end">
                                     <a href="#" class="btn btn-sm btn-outline-warning" title="Sửa">
                                         <i class="fas fa-edit"></i>
