977a16ac1bbd6998d89704dfa04356f3dc62fcbf
Văn Đạt <levandat21082005@gmail.com>
2026-03-02 16:04:19 +0700

    Edit giao diện admin

--- resources/views/Admin/partials/sidebar.blade.php
+++ resources/views/Admin/partials/sidebar.blade.php
@@ -1,14 +1,37 @@
-<div id="sidebar-wrapper" class="bg-light">
+<div id="sidebar-wrapper" class="bg-light" style="width: 18%;">
     <div class="sidebar-heading p-3 fs-4 text-body fw-bold "><i class="fas fa-user-shield text-primary"></i>
         Adminator</div>
     <div class="nav flex-column py-3  ">
         <a href="#" class="nav-link text-muted fw-bold  "><i class="fas fa-tachometer-alt"></i> Dashboard</a>
-        <a href="{{ route('admin.categories.index') }}" class="nav-link text-muted fw-bold"><i class="fas fa-box"></i> Quản lý Danh mục</a>
-        <a href="{{ route('admin.products.index') }}" class="nav-link text-muted fw-bold"><i class="fas fa-box"></i> Quản lý Sản phẩm</a>
-        <a href="{{ route('admin.orders.index') }}" class="nav-link text-muted fw-bold"><i class="fas fa-shopping-cart"></i> Đơn hàng</a>
-        <!-- <a href="#" class="nav-link text-muted fw-bold"><i class="fas fa-chart-bar"></i> Báo cáo doanh thu</a> -->
-        <a href="#" class="nav-link text-muted fw-bold"><i class="fas fa-users"></i> Quản lý User</a>
-        <a href="#" class="nav-link text-muted fw-bold mt-auto border-0"><i class="fas fa-sign-out-alt"></i>Đăng
-            xuất</a>
+
+        <a href="{{ route('admin.categories.index') }}" class="nav-link text-muted fw-bold"><i class="fas fa-box"></i>
+            Quản lý Danh mục</a>
+
+        <a href="{{ route('admin.products.index') }}" class="nav-link text-muted fw-bold"><i class="fas fa-box"></i>
+            Quản lý Sản phẩm</a>
+
+        <a href="{{ route('admin.orders.index') }}" class="nav-link text-muted fw-bold"><i
+                class="fas fa-shopping-cart"></i> Đơn hàng</a>
+
+        <div class="nav-item border-bottom pb-2">
+            <a class="nav-link text-muted fw-bold d-flex justify-content-between align-items-center"
+                data-bs-toggle="collapse" href="#accountSubmenu" role="button" aria-expanded="false">
+                <span><i class="fas fa-users-cog"></i> Quản lý Tài khoản <i class="fas fa-chevron-down small"></i></span>
+                
+            </a>
+
+            <div class="collapse ps-3 pb-2" id="accountSubmenu">
+                <a href="{{ route('admin.accounts.users.index') }}" class="nav-link text-muted py-1 fw-bold small py-3">
+                    <i class="fas fa-user"></i> Khách hàng
+                </a>
+                <a href="" class="nav-link text-muted py-1 fw-bold small py-3">
+                    <i class="fas fa-user-tie"></i> Nhân viên (Staff)
+                </a>
+                <a href="" class="nav-link text-muted py-1 fw-bold small py-3">
+                    <i class="fas fa-key"></i> Phân quyền
+                </a>
+            </div>
+        </div>
+        <a href="#" class="nav-link text-muted fw-bold mt-auto border-0"><i class="fas fa-sign-out-alt"></i>Đăng xuất</a>
     </div>
 </div>
