b37b595b184272d466fb4f46bf50181b436cb0f8
Văn Đạt <levandat21082005@gmail.com>
2026-03-10 09:15:09 +0700

    Update header

--- resources/views/Client/partials/header.blade.php
+++ resources/views/Client/partials/header.blade.php
@@ -1,19 +1,19 @@
-<div class="header d-flex align-items-center justify-content-between py-5">
+<div class="header d-flex align-items-center justify-content-between bg-success py-5">
     <div class="logo col-3 text-end">
-        <h5 class="fs-1 m-0 fw-bold">STYLEHUB</h5>
+        <h5 class="fs-1 m-0 fw-bold text-white">STYLEHUB</h5>
     </div>
 
     <div class="col-6 mx-5">
-        <div class="input-group">
-            <input type="text" class="form-control w-25" placeholder="Search...">
-            <button class="btn btn-primary">
+        <div class="input-group"> 
+            <input type="text" class="form-control w-50" placeholder="Search...">
+            <button class="btn btn-warning">
                 <i class="fas fa-search"></i>
             </button>
         </div>
     </div>
 
     <div class="col-3 text-start fs-5">
-        <a href=""><i class="fa-regular fa-user px-3 text-dark"></i></a>
-        <a href=""><i class="fa-solid fa-bag-shopping text-dark"></i></a>
+        <a href=""><i class="fa-regular fa-user px-3 text-white"></i></a>
+        <a href=""><i class="fa-solid fa-bag-shopping text-white"></i></a>
     </div>
 </div>
