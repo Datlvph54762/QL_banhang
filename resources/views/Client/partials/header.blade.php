2b82ecc6ea5dbe2ffccab2d5aad5882b2f5aabce
Văn Đạt <levandat21082005@gmail.com>
2026-04-03 16:35:47 +0700

    Khởi tạo menu  Quản lí đơn hàng cho client

--- resources/views/Client/partials/header.blade.php
+++ resources/views/Client/partials/header.blade.php
@@ -15,14 +15,40 @@
         <a href="{{ route('client.carts.index') }}"><i class="fa-solid fa-bag-shopping px-3 text-white"></i></a>
         @auth
             @if(Auth::guard('web')->check())
-                <span class="text-white fs-6 fw-bold mt-1">{{ Auth::guard('web')->user()->name }}</span>
+                <div class="dropdown">
+                    <a class="text-white fs-6 fw-bold mt-1 dropdown-toggle text-decoration-none shadow-none" href="#"
+                        role="button" id="userMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                        {{ Auth::guard('web')->user()->name }}
+                    </a>
+
+                    {{-- Menu thả xuống --}}
+                    <ul class="dropdown-menu dropdown-menu-white shadow" id="userMenu">
+                        <li>
+                            <a class="dropdown-item py-2" href="{{ route('client.accounts.orders.index') }}">
+                                <i class="fas fa-history me-2"></i> Lịch sử đơn hàng
+                            </a>
+                        </li>
+                        <li>
+                            <a class="dropdown-item py-2" href="#">
+                                <i class="fas fa-user-edit me-2"></i> Thông tin cá nhân
+                            </a>
+                        </li>
+                        <li>
+                            <hr class="dropdown-divider border-secondary">
+                        </li>
+                        <li>
+                            {{-- Form Đăng xuất lồng vào menu --}}
+                            <form action="{{ route('client.logout') }}" method="POST"
+                                onclick="return confirm('Bạn chắc chắn muốn đăng xuất?')" class="m-0">
+                                @csrf
+                                <button type="submit" class="dropdown-item text-danger py-2">
+                                    <i class="fas fa-sign-out-alt me-2"></i> Đăng xuất
+                                </button>
+                            </form>
+                        </li>
+                    </ul>
+                </div>
             @endif
-            <form action="{{ route('client.logout') }}" method="POST" onclick="return confirm('Bạn chắc chắn muốn đắng xuất?')" class="d-inline">
-                @csrf
-                <button type="submit" class="p-0 border-0 bg-transparent text-white">
-                    <i class="fas fa-sign-out-alt ps-2"></i>
-                </button>
-            </form>
         @endauth
 
         @guest
